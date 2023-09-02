46e49e8971bde9f35d17ef4620142a1d71b80733
vova-eromin2345 <105551329+vova-eromin2345@users.noreply.github.com>
2023-09-02 22:10:51 +0300

    Update sql_tools.py

--- sql_tools.py
+++ sql_tools.py
@@ -10,17 +10,21 @@ def close_db(conn, cursor):
     conn.commit()
 
 
-def read_from_db(db_path, table, *args):
-    _, cursor = connect_to_db(db_path)
+def read_from_db(db_path, table, *args, condition=""): # type of args -> str
+    conn, cursor = connect_to_db(db_path)
 
-    schema_temp = 'SELECT {} from '+ table 
-
+    schema_temp = 'SELECT {} from ' + table 
+    if condition:
+        schema_temp += f' WHERE {condition}'
+    
     if '*' in args:
-        return cursor.execute(schema_temp.format('*')).fetchall()
+        result = cursor.execute(schema_temp.format('*')).fetchall()
     else:
         args_temp = '({})'.format(','.join(args))
-        return cursor.execute(schema_temp.format(args_temp)).fetchall()
-    
+        result = cursor.execute(schema_temp.format(args_temp)).fetchall()
+    close_db(conn, cursor)
+    return result
+
 
 def write_to_db(db_path, table, wait_args:tuple, args:tuple):
     wait_args, args = tuple(wait_args), tuple(args)
@@ -35,9 +39,32 @@ def write_to_db(db_path, table, wait_args:tuple, args:tuple):
     close_db(conn, cursor)
 
 
-def create_table(db_path, table, *rows):
+def update_db(db_path, table, condition="", **columns):
+    
     conn, cursor = connect_to_db(db_path)
+        
+    columns_temp = [f'{col} = ?' for col in columns]
+    schema_temp = f'''
+    UPDATE {table}
+    SET {', '.join(columns_temp)}
+    '''
+    if condition:
+        schema_temp += f'WHERE {condition}'
 
+    
+    cursor.execute(schema_temp, tuple(columns.values()))
+    
+    close_db(conn, cursor)
+    
+
+def create_table(db_path, table, *rows:list): 
+    '''
+        example:
+        create_table('test.db', 'users', ['name', 'TEXT'], ['age', 'INTEGER'])
+    '''
+    rows = list(map(lambda x: f'{x[0]} {x[1]}', rows))
+    conn, cursor = connect_to_db(db_path)
+    
     schema = "CREATE TABLE IF NOT EXISTS {} ({})".format(table, ",".join(rows))
     cursor.execute(schema)
 
